f5afdbedd76de12a4ee05721cc9bfc3736132416
Lucas Gabriel <lucasgabrielba@gmail.com>
2022-10-24 17:38:43 -0300

    404

--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -1,17 +1,11 @@
-import pagenotfoundImage from './pagenotfound.jpg';
+import pagenotfoundImage from "./pagenotfound.jpg";
+import styles from "./index.module.css";
 
 export const PageNotFound = () => {
   return (
-
-        <div className="pageNotFound">
-          <h1>Oops..! 404 página não encontrada</h1>
-          <p>Parece que você chegou à página errada em nosso servidor</p>
-          <img
-            src={pagenotfoundImage}
-            height="500"
-            width="500"
-            alt="not found"
-          />
-        </div>
+    <div className={styles.pageNotFound}>
+      <h1>Oops..! 404 página não encontrada</h1>
+      <p>Parece que você chegou à página errada em nosso servidor</p>
+    </div>
   );
 };
